d412b5628f5635864208edaf5158d3ffcbd0b532
myeongjinKim <ab9010@naver.com>
2019-05-23 15:25:00 +0900

    Delete Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -24,12 +24,12 @@ let movies = [
   export const getMovies = () => movies;
 
   export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
   };
 
   export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length) {
       movie = cleanedMovies;
       return true;
@@ -40,7 +40,7 @@ let movies = [
 
   export const addMovie = (name, score) => {
     const newMovie = {
-      id: `${movies.length + 1}`,
+      id: parseInt(`${movies.length + 1}`),
       name,
       score
     };
